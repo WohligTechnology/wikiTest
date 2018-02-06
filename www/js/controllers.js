c6cbd939bce4d599ec8df70f457b7b261bc02abf
sohan1 <shn619@gmail.com>
2018-02-06 17:58:00 +0530

    contoller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,32 +4,72 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistsCtrl', function ($scope, $timeout) {
   $timeout(function () {
-    var demo = {
-      requiredFeatures: ["2d_tracking", "geo"],
-      isDeviceSupported: false,
-      startupConfiguration: {
-        "camera_position": "back"
+    var World = {
+      // true once data was fetched
+      initiallyLoadedData: false,
+
+      // POI-Marker asset
+      markerDrawable_idle: null,
+
+      // called to inject new POI data
+      loadPoisFromJsonData: function loadPoisFromJsonDataFn(poiData) {
+
+        /*
+        	The example Image Recognition already explained how images are loaded and displayed in the augmented reality view. This sample loads an AR.ImageResource when the World variable was defined. It will be reused for each marker that we will create afterwards.
+        */
+        World.markerDrawable_idle = new AR.ImageResource("assets/marker_idle.png");
+
+        /*
+        	Since there are additional changes concerning the marker it makes sense to extract the code to a separate Marker class (see marker.js). Parts of the code are moved from loadPoisFromJsonData to the Marker-class: the creation of the AR.GeoLocation, the creation of the AR.ImageDrawable and the creation of the AR.GeoObject. Then instantiate the Marker in the function loadPoisFromJsonData:
+        */
+        var marker = new Marker(poiData);
+
+        // Updates status message as a user feedback that everything was loaded properly.
+        World.updateStatusMessage('1 place loaded');
       },
-      onDeviceReady: function () {
-        demo.wikitudePlugin.isDeviceSupported(demo.onDeviceSupported, demo.onDeviceNotSupported, demo.requiredFeatures);
+
+      // updates status message shown in small "i"-button aligned bottom center
+      updateStatusMessage: function updateStatusMessageFn(message, isWarning) {
+
+        var themeToUse = isWarning ? "e" : "c";
+        var iconToUse = isWarning ? "alert" : "info";
+
+        $("#status-message").html(message);
+        $("#popupInfoButton").buttonMarkup({
+          theme: themeToUse
+        });
+        $("#popupInfoButton").buttonMarkup({
+          icon: iconToUse
+        });
       },
-      onDeviceSupported: function () {
-        demo.wikitudePlugin.loadARchitectWorld(
-          app.onARExperienceLoadedSuccessful,
-          app.onARExperienceLoadError,
-          app.arExperienceUrl,
-          app.requiredFeatures,
-          app.startupConfiguration
-        );
+
+      // location updates, fired every time you call architectView.setLocation() in native environment
+      locationChanged: function locationChangedFn(lat, lon, alt, acc) {
+
+        /*
+        	The custom function World.onLocationChanged checks with the flag World.initiallyLoadedData if the function was already called. With the first call of World.onLocationChanged an object that contains geo information will be created which will be later used to create a marker using the World.loadPoisFromJsonData function.
+        */
+        if (!World.initiallyLoadedData) {
+          // creates a poi object with a random location near the user's location
+          var poiData = {
+            "id": 1,
+            "longitude": (lon + (Math.random() / 5 - 0.1)),
+            "latitude": (lat + (Math.random() / 5 - 0.1)),
+            "altitude": 100.0,
+            "description": "This is the description of POI#1",
+            "title": "POI#1"
+          };
+
+          World.loadPoisFromJsonData(poiData);
+          World.initiallyLoadedData = true;
+        }
       },
-      onARExperienceLoadedSuccessful: function (loadedURL) {
-        console.log('onARExperienceLoadedSuccessful');
-        demo.wikitudePlugin.callJavaScript('createCircle(new AR.RelativeLocation(null, -10, 0), \'#97FF18\');');
-      },
-      onARExperienceLoadError: function (errorMessage) {
-        console.log("onARExperienceLoadError");
-      }
     };
+
+    /* 
+    	Set a custom function where location changes are forwarded to. There is also a possibility to set AR.context.onLocationChanged to null. In this case the function will not be called anymore and no further location updates will be received. 
+    */
+    AR.context.onLocationChanged = World.locationChanged;
   }, 100);
 
 });
